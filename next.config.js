7706e0482e6137e2649a9269f620e0602c6ab9e5
ADRIAN BG <154455690+ADRIANBG2325@users.noreply.github.com>
2025-06-23 16:32:41 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,6 +9,27 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization, Accept, Origin, X-Requested-With",
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
